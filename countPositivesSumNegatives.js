33b4f0d3f9c61d3b31b1a8f16cc00c1c738857d2
Valeria Alcaraz Areco <vale.alcaraz14@gmail.com>
2021-09-15 09:36:34 -0400

    Instructions added

--- countPositivesSumNegatives.js
+++ countPositivesSumNegatives.js
@@ -1,30 +1,14 @@
+// Given an array of integers.
+
+// Return an array, where the first element is the count of positives numbers and the second element is sum of negative numbers.
+
+// If the input array is empty or null, return an empty array.
+
 // For input [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15], you should return [10, -65].
 
 const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, -11, -12, -13, -14, -15];
-
-// function countPositivesSumNegatives(input) {
     
-//     let positive = [];
-//     let negative = [];
-
-//     for (let number of input){
-//         if (number>=0){positive.push(number)};
-//         if (number<0){negative.push(number)};
-//     }  
-//     let aux = (acc,num) => acc + num;
-//     let negativeSum = negative.reduce(aux)
-
-//     console.log([positive.length,negativeSum])  
-
-//         // return ?;
-    
-//   }
-
-//   countPositivesSumNegatives(input)
-    
-
 function countPositivesSumNegatives(input) {
-
 
     if(!input || input === null) {
         return input=0
